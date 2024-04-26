65b5704e8c2ceeb08a9d99577ad1ca6a330321ae
perepalacin <perepalacin@gmail.com>
2024-04-26 22:11:35 +0200

    deleted extra boiler plate files

--- src/types.ts
+++ src/types.ts
@@ -1,33 +1,3 @@
-export enum Brand {
-    Tissot = "Tissot",
-    Seiko = "Seiko",
-    Citizen = "Citizen",
-    Casio = "Casio",
-    Orient = "Orient",
-}
-
-export enum Mechanism {
-    Quarz = "Quarz",
-    Automatic = "Automatic"
-}
-
-export interface WatchEntry {
-    id: string,
-    created_at: string,
-    name: string,
-    description: string,
-    brand: Brand,
-    mechanism: Mechanism,
-}
-
-export interface TeamMemberDetails {
-    name: string;
-    picture: string;
-}
-export interface TeamMemberProps {
-    teamName: string;
-    members: TeamMemberDetails[];
-}
 
 export interface absences_data {
     id: number | undefined;
@@ -36,8 +6,11 @@ export interface absences_data {
     finish: Date;
     employee_id: number;
 }
-
-// export type SummarizedWatchEntry = Pick<DiaryEntry, "id" | "name" | "created_at">;
-export type SummarizedWatchEntry = Omit<WatchEntry, 'description'>;
-
-export type NewWatchEntry = Omit<WatchEntry, 'id'>;
+export interface TeamMemberDetails {
+    name: string;
+    picture: string;
+}
+export interface TeamMemberProps {
+    teamName: string;
+    members: TeamMemberDetails[];
+}
